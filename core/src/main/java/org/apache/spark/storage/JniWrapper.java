ae09510b84922282adbd8ea339329d12c4315044
ymiyazato <ymiyazato@asg.cs.tuat.ac.jp>
2020-12-09 03:05:45 +0900

    madvise library

--- core/src/main/java/org/apache/spark/storage/JniWrapper.java
+++ core/src/main/java/org/apache/spark/storage/JniWrapper.java
@@ -6,10 +6,10 @@ public class JniWrapper{
 //    static {
 //        System.loadLibrary(LIB_NAME);
 //    }
-    private native int mymadvise(long startAddr, long hugePageSize);
-    private native int returnzero(long num, long numz);
+    private native int mymadvise(long addr, long size);
+    private native int returnzero(long startAddr, long hugePageSize);
     public int call_madvise(long startAddr, long hugePageSize) {
         System.out.println("call madvise by madvise");
-        return (mymadvise(startAddr, hugePageSize));
+        return (returnzero(startAddr, hugePageSize));
     }
 }
